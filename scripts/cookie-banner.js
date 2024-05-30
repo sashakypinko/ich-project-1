3adef6690a9257de729d5ed7af005bb3e8a829cb
Alexander <sashakypinko@gmail.com>
2024-05-30 10:12:43 +0200

    added footer and cookie banner

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -1,8 +1,13 @@
 const cookieBanner = document.querySelector('.cookies-banner');
 const closeCookieBannerButtons = document.querySelectorAll('.js-close-cookie-banner');
+const cookieBannerTransitionDuration = window.getComputedStyle(cookieBanner).transitionDuration.slice(0, -1);
 
 closeCookieBannerButtons.forEach(button => {
   button.addEventListener('click', () => {
-    cookieBanner.style.display = 'none';
-  })
+    cookieBanner.style.bottom = `-${cookieBanner.scrollHeight + 10}px`;
+
+    setTimeout(() => {
+      cookieBanner.style.display = 'none';
+    }, cookieBannerTransitionDuration * 1000);
+  });
 });
